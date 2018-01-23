bfb4e724de0f354f35186aebca09f3e7bbc0b507
Zhou <zhou.shen@marquette.edu>
2018-01-22 21:03:35 -0600

    Update and rename Assignment1.java to Different kinds of basic objects.java

--- Different kinds of basic objects.java
+++ Different kinds of basic objects.java
@@ -9,7 +9,7 @@ import javafx.scene.paint.Color;
 /**
  * Assignment 1
  * Write a JavaFX application which displays a picture
- * with different kinds of images and colors
+ * 
  * @author Zhou Shen
  * @version Jan 2018
  */
@@ -71,6 +71,16 @@ public class Assignment1 extends Application{
         gc.fillOval(300, 400, 40, 40);
         gc.setFill(Color.DEEPSKYBLUE);
         gc.fillOval(350, 400, 40, 40);
+        
+        //draw different kinds of polygon
+        gc.setFill(Color.RED);
+        gc.fillPolygon(new double[]{200, 240, 200},new double[]{110, 110, 140}, 3);//triangle 
+        
+        gc.setFill(Color.PURPLE);
+        double xpoints[] = {25, 145, 25, 145, 25};  //xPoints - array containing the x coordinates of the polygon's points or null.
+        double ypoints[] = {25, 25, 145, 145, 25};  //yPoints - array containing the y coordinates of the polygon's points or null.
+        int npoints = 5;   //nPoints - the number of points that make the polygon.
+        gc.fillPolygon(xpoints, ypoints, npoints);
         
     }
 }
